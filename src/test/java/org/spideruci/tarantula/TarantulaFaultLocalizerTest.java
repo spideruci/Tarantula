35c16caad41e1d3fc8068dcd9331aa4fd3197903
VijayKrishna Palepu <vijay.0288@gmail.com>
2015-06-01 20:21:58 -0700

    Test Scafolding and Travis/Coveralls Setup

---
+++ src/test/java/org/spideruci/tarantula/TarantulaFaultLocalizerTest.java
@@ -0,0 +1,40 @@
+package org.spideruci.tarantula;
+
+import static org.junit.Assert.*;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class TarantulaFaultLocalizerTest {
+
+  @Test @Ignore
+  public void testCalculateBadTestCoverage() {
+    fail("Not yet implemented");
+  }
+
+  @Test @Ignore
+  public void testCalculateTotalLiveFailAndPass() {
+    fail("Not yet implemented");
+  }
+
+  @Test @Ignore
+  public void testCalculateOrigFailAndPass() {
+    fail("Not yet implemented");
+  }
+
+  @Test @Ignore
+  public void testCalculatePassOnStmtAndFailOnStmt() {
+    fail("Not yet implemented");
+  }
+
+  @Test @Ignore
+  public void testCalculatePassRatioAndFailRatio() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testCalculateSuspiciousnessAndConfidence() {
+    fail("Not yet implemented");
+  }
+
+}
